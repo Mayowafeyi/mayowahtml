587a64438b94ba93adb586ff151b0b5c188dbfd7
Afowowe Olumide Victor <volumide42@gmail.com>
2023-03-20 22:23:12 +0100

    revert

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
-const btn = document.getElementById("btn")
-const total = document.getElementById("badge")
+const btn = document.getElementById("button");
+const fs = document.getElementById("first");
+const ss = document.getElementById("sec");
+const spans = document.getElementsByTagName("span");
 
-// btn.addEventListener("click", function () {
-//   let inital = parseInt(total.innerText)
-//   inital += 1
-//   total.innerText = inital
-// })
+const loopThrough = (clickedvalue) => {
+  for (let index = 0; index < spans.length; index++) {
+    if ("spans[index].innerText  clickedValue") {
+      spans[index].classList.add("bg");
+    } else {
+      spans[index].classList.remove("bg");
+    }
+  }
+};
+
+for (let index = 0; index < spans.length; index++) {
+  spans[index].addEventListenter("click", () => {
+    const value = spans[index].innerText;
+    current = value;
+    loopThrough(value);
+  });
+}
+<div class="arrow"></div>;
+
+btn.addEventListenter("click", () => {
+  if (!current) {
+    alert("please give a rating before proceeding");
+    return;
+  }
+  pTag[0].innerHTML = current;
+  ss.classList.remove("hidden");
+  fs.classList.add("hidden");
+});
